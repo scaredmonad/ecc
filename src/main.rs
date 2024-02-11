46366e8cb53f92092013d0fa5b170b1058a195a9
scaredmonad <voidptr9@gmail.com>
2024-02-11 20:38:16 +0300

    feat: lexer covers symbols

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,250 @@
+#![allow(dead_code)]
+use std::str::Chars;
+
+#[derive(Debug, PartialEq)]
+enum TokenType {
+    Identifier(String),
+    IntLiteral(i64),
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Semicolon,
+    Plus,
+    Minus,
+    Equal,
+    Mul,
+    Div,
+    EOF,
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+}
+
+struct Lexer<'a> {
+    input: Chars<'a>,
+    current_char: Option<char>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Self {
+        let mut chars = input.chars();
+        let current_char = chars.next();
+        Lexer {
+            input: chars,
+            current_char,
+        }
+    }
+
+    fn advance(&mut self) {
+        self.current_char = self.input.next();
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.current_char {
+            if c.is_whitespace() {
+                self.advance();
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn consume_identifier(&mut self) -> String {
+        let mut identifier = String::new();
+        while let Some(c) = self.current_char {
+            if c.is_alphanumeric() || c == '_' {
+                identifier.push(c);
+                self.advance();
+            } else {
+                break;
+            }
+        }
+        identifier
+    }
+
+    fn consume_number(&mut self) -> i64 {
+        let mut number = String::new();
+        while let Some(c) = self.current_char {
+            if c.is_digit(10) {
+                number.push(c);
+                self.advance();
+            } else {
+                break;
+            }
+        }
+        number.parse().unwrap_or(0)
+    }
+
+    fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        let token = match self.current_char {
+            Some('(') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::LeftParen,
+                    lexeme: "(".to_string(),
+                }
+            }
+
+            Some(')') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::RightParen,
+                    lexeme: ")".to_string(),
+                }
+            }
+
+            Some('{') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::LeftBrace,
+                    lexeme: "{".to_string(),
+                }
+            }
+
+            Some('}') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::RightBrace,
+                    lexeme: "}".to_string(),
+                }
+            }
+
+            Some('[') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::LeftBracket,
+                    lexeme: "[".to_string(),
+                }
+            }
+
+            Some(']') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::RightBracket,
+                    lexeme: "]".to_string(),
+                }
+            }
+
+            Some(',') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Comma,
+                    lexeme: ",".to_string(),
+                }
+            }
+
+            Some(';') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Semicolon,
+                    lexeme: ";".to_string(),
+                }
+            }
+
+            Some('+') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Plus,
+                    lexeme: "+".to_string(),
+                }
+            }
+
+            Some('-') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Minus,
+                    lexeme: "-".to_string(),
+                }
+            }
+
+            Some('*') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Mul,
+                    lexeme: "*".to_string(),
+                }
+            }
+
+            Some('/') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Div,
+                    lexeme: "/".to_string(),
+                }
+            }
+
+            Some('=') => {
+                self.advance();
+
+                Token {
+                    token_type: TokenType::Equal,
+                    lexeme: "=".to_string(),
+                }
+            }
+
+            Some(c) if c.is_alphabetic() => {
+                let identifier = self.consume_identifier();
+
+                Token {
+                    token_type: TokenType::Identifier(identifier.clone()),
+                    lexeme: identifier,
+                }
+            }
+
+            Some(c) if c.is_digit(10) => {
+                let number = self.consume_number();
+
+                Token {
+                    token_type: TokenType::IntLiteral(number),
+                    lexeme: number.to_string(),
+                }
+            }
+
+            None => Token {
+                token_type: TokenType::EOF,
+                lexeme: "".to_string(),
+            },
+
+            _ => {
+                panic!("Invalid character found: {:?}", self.current_char);
+            }
+        };
+
+        token
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "int add(int a, int b) { return a + b; } int main() { int[] c = [add(5, 10)]; }";
+    let mut lexer = Lexer::new(input);
+
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+
+        if token.token_type == TokenType::EOF {
+            break;
+        }
+    }
 }
